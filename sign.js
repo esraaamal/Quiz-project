1b2b078015c4470c9676c9bd99bcdbb10f5cba78
esraaamal <ma.esraa@hotmail.com>
2021-10-24 23:11:47 +0300

    f

--- sign.js
+++ sign.js
@@ -1,24 +1,15 @@
-const sign_Btn = document.getElementById("signup_btn");
-const form = document.getElementById("form");
-// sign_Btn.addEventListener("click",sginFunction);
-let welcomeQuiz = document.querySelector(".welcomeQuiz");
+const sign_Btn=document.getElementById("signup_btn");
+const form=document.getElementById("form");
+let welcomeQuiz=document.querySelector('.welcomeQuiz')
 
-////popUp////
-const registerNow = (e) => {
-  document.querySelector(".popUpSignUp1").style.display = "flex";
-  e.preventDefault();
-};
-const removePopup = (e) => {
-  document.querySelector(".popUpSignUp1").style.display = "none";
-};
 
-///////////popUp////
+
 
 //username
 
 const input_username = document.getElementById("input_username");
 const username_help = document.getElementById("username_help");
-const username_format = /^[a-zA-Z0-9_$\.]{4,16}$/;
+const username_format= /^[a-zA-Z0-9_$\.]{4,16}$/;
 //email
 
 const input_email = document.getElementById("input_email");
@@ -31,107 +22,129 @@ const pass_help = document.getElementById("pass_help");
 
 const format_pass = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
-input_username.onkeyup = function () {
-  //userName
-  if (input_username.value === "") {
-    username_help.innerHTML = "username is empty";
-    username_help.style.color = "red";
-  } else {
-    if (input_username.value.match(username_format)) {
-      username_help.innerHTML = "";
-      username_help.style.color = "green";
-    } else {
-      username_help.innerHTML = "username not valid";
-      username_help.style.color = "red";
-    }
-  }
-};
+   
+sign_Btn.addEventListener("click",sginFunction);
 
-//email
-input_email.onkeyup = function () {
-  if (input_email.value === "") {
-    email_help.innerHTML = "email is empty";
-    email_help.style.color = "red";
-  } else {
-    if (input_email.value.match(email_format)) {
-      email_help.innerHTML = "";
-      email_help.style.color = "green";
-    } else {
-      email_help.innerHTML = "email not valid";
-      email_help.style.color = "red";
+
+function sginFunction(e) {
+
+    //userName
+    if(input_username.value===""){
+
+        username_help.innerHTML="username is empty";
+        username_help.style.color="red"
+        
+     }
+     else {
+    
+        
+        if (input_username.value.match(username_format)){
+           
+            username_help.innerHTML="";
+            username_help.style.color="green"
+        }
+        else {
+            
+            username_help.innerHTML="username not valid";
+            username_help.style.color="red"
+        }
     }
-  }
-};
+
+    //email
+   
+ if(input_email.value===""){
+
+    email_help.innerHTML="email is empty";
+    email_help.style.color="red"
+
+ }
+ else {
+
+    
+    if (input_email.value.match(email_format)){
+       
+        email_help.innerHTML="";
+        email_help.style.color="green"
+
+
+    }
+    else {
+        
+        email_help.innerHTML="email not valid";
+        email_help.style.color="red"
+    }
+}
+
+
 
 //pass
-input_pass.onkeyup = function () {
-  if (input_pass.value === "") {
-    pass_help.innerHTML = "Password is empty ";
-    pass_help.style.color = "red";
-  } else {
-    if (input_pass.value.match(format_pass)) {
-      pass_help.innerHTML = "";
-      pass_help.style.color = "green";
-    } else {
-      pass_help.innerHTML = "Password Not Valid";
-
-      pass_help.style.color = "red";
-      form.action = "#form";
+if(input_pass.value===""){
+   
+    pass_help.innerHTML="Password is empty ";
+    pass_help.style.color="red";
+}
+else{
+  if(input_pass.value.match(format_pass)){
+    pass_help.innerHTML="";
+    pass_help.style.color="green";
     }
-  }
-  // e.preventDefault()
-};
-//
+    else{
+       
+        pass_help.innerHTML="Password Not Valid";  
+        pass_help.style.color="red";
+       
+    }
+
+}
+// e.preventDefault()
+
+}
+
+
 ////how to store the data from user input ! ///////
 
-const signUp = (e) => {
-  let formData = JSON.parse(localStorage.getItem("formData")) || [];
-  let exist =
-    formData.length &&
-    JSON.parse(localStorage.getItem("formData")).some(
-      (data) => data.email.toLowerCase() == input_email.value.toLowerCase()
-    );
+const signUp=(e)=>{
+    let formData=JSON.parse(localStorage.getItem('formData')) ||[]
+    let exist=formData.length &&
+    JSON.parse(localStorage.getItem('formData')).some(data=>data.email.toLowerCase()==input_email.value.toLowerCase())
+    
+    if(!exist){
+        formData.push(
+            {    
+                userName:input_username.value,
+                email:input_email.value,
+                password:input_pass.value
+            })
 
-  if (!exist) {
-    formData.push({
-      userName: input_username.value,
-      email: input_email.value,
-      password: input_pass.value,
-    });
+            localStorage.setItem('formData',JSON.stringify(formData))
+            console.log(JSON.parse(localStorage.getItem('formData')))
+            form.reset()
+            input_username.focus()
 
-    localStorage.setItem("formData", JSON.stringify(formData));
-    console.log(JSON.parse(localStorage.getItem("formData")));
-    form.reset();
-    input_username.focus();
-    document.getElementById("form").action = "../sign_in.html";
-  } else {
-    alert("you already have an account");
-    document.getElementById("form").action = "../sign_in.html";
-  }
+    }
+    else{
+        alert('you already have an account')
+    }
 
-  // e.preventDefault()
-};
-function loadName() {
-  console.log(localStorage.getItem("username"));
-  let nameSign = localStorage.getItem("username");
-  try {
-    var x = nameSign.length;
-  } catch {
-    console.log("");
-  }
-  if (!x) {
-    welcomeQuiz.innerHTML =
-      "Welcome to Web Developer position" +
-      "<br>" +
-      "please Answer all the Quizzes to get the interview Job !";
-  } else {
-      try{
-    welcomeQuiz.innerHTML = `Welcome to Web Developer position <mark>${nameSign}</mark> 
-    <br> please Answer all the Quizzes to get the interview Job !`;
-      }
-      catch {console.log("")}
-  }
+
+    e.preventDefault()
+
+
 }
-loadName();
+
 
 ///////////Sign In Page'////////////
+function loadName(){
+  console.log(localStorage.getItem('username'))
+let nameSign=localStorage.getItem('username')
+if(!nameSign.length){
+  welcomeQuiz.innerHTML= "Welcome to Web Developer position"+ "<br>"
+ + "please Answer all the Quizzes to get the interview Job !"
+}
+else{
+  welcomeQuiz.innerHTML=`Welcome to Web Developer position <mark>${nameSign}</mark> 
+  <br> please Answer all the Quizzes to get the interview Job !`
+}
+
+}
+loadName()
